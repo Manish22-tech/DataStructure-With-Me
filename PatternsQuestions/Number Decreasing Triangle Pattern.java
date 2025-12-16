de42e34796921368eed0dc3d69cce2910fb01107
Manish Yadav <yadavmanish9617@gmail.com>
2025-12-16 18:52:36 +0530

    Add Number Decreasing Triangle Pattern program
    Implement a number decreasing triangle pattern in Java.

---
+++ PatternsQuestions/Number Decreasing Triangle Pattern.java
@@ -0,0 +1,20 @@
+package Pattern;
+
+public class Example2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*output 
+12345
+1234
+123
+12
+1
+*/
